bb305c5e3fcd91d6d9910be4470f0385b1de7eae
Jean Pierre <jeanpierre.webdev@gmail.com>
2024-11-11 00:07:08 -0500

    Remove Playwright testing setup and configurations

--- tests/navigation.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test'
-
-test('navigation smoke test', async ({ page }) => {
-	// start at the home page
-	await page.goto('/auth')
-
-	await expect(page).toHaveTitle(/Slack/)
-	await expect(page.getByRole('heading', { name: 'Login to continue', level: 3 })).toBeVisible()
-})
